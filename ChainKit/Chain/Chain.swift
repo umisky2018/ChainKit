24a2fdb3154cc6d2216bcde323b6fbc0a64af7bb
umisky <chenshu.water@gmail.com>
2020-05-23 01:14:47 +0800

    add namespace

---
+++ ChainKit/Chain/Chain.swift
@@ -0,0 +1,43 @@
+//
+//  Chain.swift
+//  ChainKit
+//
+//  Created by umisky on 2020/5/23.
+//  Copyright © 2020 chenshu. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - namespace
+
+public protocol ChainCompatible {
+    
+    associatedtype ChainBase
+    
+    static var chain: Chain<ChainBase>.Type { get set }
+    
+    var chain: Chain<ChainBase> { set get }
+}
+
+public extension ChainCompatible {
+    
+    static var chain: Chain<Self>.Type {
+        get { return Chain.self }
+        set { }
+    }
+    
+    var chain: Chain<Self> {
+        get { return Chain(self) }
+        set { }
+    }
+}
+
+/// wrapper
+public struct Chain<Base> {
+    
+    public var base: Base
+    
+    init(_ base: Base) {
+        self.base = base
+    }
+}
