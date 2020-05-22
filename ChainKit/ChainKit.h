e733fe150ba69fcc42bc5597c34a1c8459868d4e
umisky <chenshu.water@gmail.com>
2020-05-23 01:08:11 +0800

    Initial

---
+++ ChainKit/ChainKit.h
@@ -0,0 +1,19 @@
+//
+//  ChainKit.h
+//  ChainKit
+//
+//  Created by umisky on 2020/5/23.
+//  Copyright © 2020 chenshu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChainKit.
+FOUNDATION_EXPORT double ChainKitVersionNumber;
+
+//! Project version string for ChainKit.
+FOUNDATION_EXPORT const unsigned char ChainKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChainKit/PublicHeader.h>
+
+
